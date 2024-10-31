d2a56a4fc38f97417e9e8be115e0c098e1a0893d
Gasoid <gasoid@gmail.com>
2024-10-31 12:28:17 +0100

    added location

--- parsers.go
+++ parsers.go
@@ -2,5 +2,6 @@ package main
 
 import (
 	_ "github.com/Gasoid/regular-go-bot/parsers"
+	_ "github.com/Gasoid/regular-go-bot/parsers/location"
 	_ "github.com/Gasoid/regular-go-bot/parsers/speech"
 )
